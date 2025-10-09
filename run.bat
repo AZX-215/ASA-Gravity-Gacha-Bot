383a44973174dd37174eaa228b7cdc80b644ec1b
AZX <125766660+AZX-215@users.noreply.github.com>
2025-10-09 00:34:24 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,7 @@
 @echo off
 setlocal
-if not exist venv\Scripts\python.exe (
-  py -3.11 -m venv venv || goto :eof
-)
+cd /d %~dp0
+if not exist venv\Scripts\python.exe py -3.11 -m venv venv
 call venv\Scripts\activate.bat
-python -m pip --disable-pip-version-check -q install -r requirements.txt
+python -m pip -q install -r requirements.txt
 python main.py
