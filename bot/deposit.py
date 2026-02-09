56ed3daf2ac50993f55d5d33077465a6cff724c2
AZX <125766660+AZX-215@users.noreply.github.com>
2026-02-09 16:06:47 -0500

    Update deposit.py

--- bot/deposit.py
+++ bot/deposit.py
@@ -156,11 +156,11 @@ def dedi_deposit_custom_1(height):
         time.sleep(0.3*settings.lag_offset)
         utils.press_key("Use")
         time.sleep(0.3*settings.lag_offset)
-        utils.turn_down(15)
+        utils.turn_down(30)
         time.sleep(0.3*settings.lag_offset)
         utils.press_key("Use")
         time.sleep(0.3*settings.lag_offset)
-        utils.turn_up(30)
+        utils.turn_up(45)
         time.sleep(0.3*settings.lag_offset)
 
     else:
@@ -343,5 +343,6 @@ def deposit_all(metadata):
         collect_grindables(grindables_metadata)
     else:
         drop_useless()
+
 
 
