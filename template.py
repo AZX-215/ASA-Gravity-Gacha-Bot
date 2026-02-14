faffd955627f2b3b208d7fdc91588159600c2530
AZX <125766660+AZX-215@users.noreply.github.com>
2026-02-13 22:03:43 -0500

    Add files via upload

--- template.py
+++ template.py
@@ -100,7 +100,7 @@ roi_regions = {
     "forge_ready_for_activation": {"start_x":1100, "start_y":245 ,"width":355 ,"height":150},
     "forge_already_on": {"start_x":1100, "start_y":245 ,"width":355 ,"height":150},
     "forge_not_ready_no_fuel": {"start_x":1100, "start_y":245 ,"width":355 ,"height":150},
-    "access_inv":{"start_x":550, "start_y":450 ,"width":1670 ,"height":880}
+    "access_inv":{"start_x":550, "start_y":450 ,"width":1670 ,"height":880},
     "auto_stack": {"start_x":2050, "start_y":140 ,"width":480 ,"height":260},
     "auto_stack_icon": {"start_x":2050, "start_y":140 ,"width":480 ,"height":260},
 }
