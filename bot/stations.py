7dae487e1d498bdcb4ce029c47ff654a64ca220f
AZX <125766660+AZX-215@users.noreply.github.com>
2026-02-13 21:57:38 -0500

    Add files via upload

--- bot/stations.py
+++ bot/stations.py
@@ -7,7 +7,7 @@ import utils
 from ASA.structures import bed , teleporter , inventory
 from ASA.player import buffs , console , player_state , tribelog , player_inventory
 from ASA.stations import custom_stations
-from bot import config , deposit , gacha , iguanadon , pego , withdrawal 
+from bot import config , deposit , gacha , iguanadon , pego , withdrawal
 from crafting.ARB import megalab as megalab_crafting
 from crafting.ARB import steam_forge
 from abc import ABC ,abstractmethod
@@ -19,7 +19,7 @@ berry_station = True
 class base_task(ABC):
     def __init__(self):
         self.has_run_before = False
-        
+
     @abstractmethod
     def execute(self):
         pass
@@ -29,7 +29,7 @@ class base_task(ABC):
     @abstractmethod
     def get_requeue_delay(self):
         pass
-    
+
     def mark_as_run(self):
         self.has_run_before = True
 
@@ -45,7 +45,7 @@ class gacha_station(base_task):
         player_state.check_state()
         global berry_station
         global last_berry
-        
+
         temp = False
         time_between = time.time() - last_berry
 
@@ -55,18 +55,18 @@ class gacha_station(base_task):
         berry_metadata = custom_stations.get_station_metadata(settings.berry_station)
         iguanadon_metadata = custom_stations.get_station_metadata(settings.iguanadon)
 
-        if (berry_station or time_between > config.time_to_reberry*60*60): # if time is greater than 4 hours since the last time you went to berry station 
-            teleporter.teleport_not_default(berry_metadata)                    # or if berry station is true( when you go to tekpod and drop all ) and the time between has been longer than 30 mins since youve last been 
-            if settings.external_berry: 
+        if (berry_station or time_between > config.time_to_reberry*60*60): # if time is greater than 4 hours since the last time you went to berry station
+            teleporter.teleport_not_default(berry_metadata)                    # or if berry station is true( when you go to tekpod and drop all ) and the time between has been longer than 30 mins since youve last been
+            if settings.external_berry:
                 logs.logger.debug("sleeping for 20 seconds as external")
                 time.sleep(20)#letting station spawn in if you have to tp away
             iguanadon.berry_station()
             last_berry = time.time()
             berry_station = False
             temp = True
-        
+
         teleporter.teleport_not_default(iguanadon_metadata) # iguanadon is a centeral tp
-        
+
         if settings.external_berry and temp: # quick fix for level 1 bug
             logs.logger.debug("reconnecting because of level 1 bug - you chose external berry will sleep for 60 seconds as a way to ensure that we are fully loaded in")
             console.console_write("reconnect")
@@ -80,13 +80,13 @@ class gacha_station(base_task):
     def get_priority_level(self):
         # Shifted to keep room for crafting tasks between pego and gachas.
         return 4
-    
+
     def get_requeue_delay(self):
         if settings.seeds_230:
-            delay = 10700  # should take about this amount of time to do 230 slots of seeds 
+            delay = 10700  # should take about this amount of time to do 230 slots of seeds
         else:
             delay = 6600    # delay can be constant as it will be the same for all gachas 142 stacks took 110 mins
-        return delay 
+        return delay
 
 class pego_station(base_task):
     def __init__(self,name,teleporter_name,delay):
@@ -97,7 +97,7 @@ class pego_station(base_task):
 
     def execute(self):
         player_state.check_state()
-        
+
         pego_metadata = custom_stations.get_station_metadata(self.teleporter_name)
         dropoff_metadata = custom_stations.get_station_metadata(settings.drop_off)
 
@@ -125,12 +125,12 @@ class pego_station(base_task):
             logs.logger.info(f"bot has no crystals in hotbar we are skipping the deposit step")
 
     def get_priority_level(self):
-        return 2 # highest prio level as we cant have these get capped 
+        return 2 # highest prio level as we cant have these get capped
 
     def get_requeue_delay(self):
         return self.delay # delay cannot be constant as stations can cover different amounts of space each |||| 2 stacks of berries to 1 crystal 4 gachas to 1 pego
-    
-    
+
+
 class sparkpowder_station(base_task):
 
     def __init__(self, name, teleporter_name, delay=0, deposit_height=3, initial_delay=0):
@@ -585,7 +585,7 @@ class render_station(base_task):
     def __init__(self):
         super().__init__()
         self.name = settings.bed_spawn
-        
+
     def execute(self):
             # Render station is a home/idle location.
             # Only run the full render workflow when render_flag is set elsewhere (e.g., via command).
@@ -611,12 +611,12 @@ class render_station(base_task):
             player_state.enter_tekpod()
             station.render_flag = False
 
-def get_priority_level(self):
+    def get_priority_level(self):
         return 8
 
     def get_requeue_delay(self):
-        return 90 # after triggered we will wait for 60 seconds reduces the amount of cpu usage 
-    
+        return 90 # after triggered we will wait for 60 seconds reduces the amount of cpu usage
+
 class snail_pheonix(base_task):
     def __init__(self,name,teleporter_name,direction,depo):
         super().__init__()
@@ -638,7 +638,7 @@ class snail_pheonix(base_task):
         time.sleep(0.2)
         deposit.dedi_deposit(settings.height_ele)
         time.sleep(0.2)
-        
+
     def get_priority_level(self):
         # Shifted to remain after normal gachas.
         return 5
@@ -658,9 +658,9 @@ class pause(base_task):
         tribelog.open()
         time.sleep(self.time)
         bot.render.leave_tekpod()
-        
+
     def get_priority_level(self):
         return 1
 
     def get_requeue_delay(self):
-        return 0  
+        return 0
